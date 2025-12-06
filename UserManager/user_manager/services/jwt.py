7acd7db1b0f391e726ebe408c3ed1160e7935642
jenjamc <jenjamak@gmail.com>
2025-12-06 17:40:09 +0200

    clean up

--- UserManager/user_manager/services/jwt.py
+++ UserManager/user_manager/services/jwt.py
@@ -2,13 +2,14 @@ from datetime import datetime
 from datetime import timedelta
 
 import jwt
+from sqlalchemy import Column
 
 from user_manager import settings
 
 
 class JwtService:
     @staticmethod
-    def create_jwt_token(user_id: int) -> str:
+    def create_jwt_token(user_id: Column[int]) -> str:
         expire = datetime.now(tz=None) + timedelta(minutes=settings.ACCESS_TOKEN_EXPIRE_MINUTES)
         token_data = {'sub': str(user_id), 'exp': expire}
         return jwt.encode(token_data, settings.SECRET_KEY, algorithm=settings.ALGORITHM)
