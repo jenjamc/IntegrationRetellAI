3d389882eda5a5f6ce01c8305912a1e172d66e7d
jenjamc <jenjamak@gmail.com>
2025-12-06 17:06:06 +0200

    clean up

--- UserManager/user_manager/settings/db.py
+++ UserManager/user_manager/settings/db.py
@@ -5,6 +5,5 @@ from sqlalchemy.orm import sessionmaker
 from user_manager.settings.conf import settings
 
 
-print('settings.sqlalchemy_database_urisettings.sqlalchemy_database_uri', settings.sqlalchemy_database_uri)
 engine = create_async_engine(settings.sqlalchemy_database_uri)
 async_session = sessionmaker(None, expire_on_commit=False, class_=AsyncSession)
